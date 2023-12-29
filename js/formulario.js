2b265de5d3f26211619174e4b6085b88f1f0b7a9
FaariD01 <moohannafarid@gmail.com>
2023-12-29 02:26:50 -0300

    Soluciono problema de linkeo foto carrito y link page

--- js/formulario.js
+++ js/formulario.js
@@ -1,11 +1,31 @@
-const formularioElement = document.getElementById("formulario");
+const formulario = document.getElementById("formulario");
 
 const procesaTodo = (event) => {
+    /*Para una accion predeterminada del evento*/
+    event.preventDefault();
+    /*constructor que crea un objeto de tipo FormData */
+   const datos = new FormData(event.target);
+   
+    /* El método Object.fromEntries() transforma una lista de pares con [clave-valor] en un objeto.*/
+   const datosCompletos = Object.fromEntries(datos.entries());
+   console.log(JSON.stringify(datosCompletos));
+   
+   localStorage.setItem("datosFormulario",JSON.stringify([datosCompletos]));
 
+   }
+
+   const procesaDatos = (event) => {
+    /*Para una accion predeterminada del evento*/
     event.preventDefault();
 
+    /*constructor que crea un objeto de tipo FormData */
     const datos = new FormData(event.target);
-    console.log(event.target);
 
+    /*El metodo get retorna el valor associado con la clave del objeto FormData */
+    const correo = datos.get('email');   
+    console.log({correo});
 
 }
+
+
+formulario.addEventListener('submit', procesaTodo());
